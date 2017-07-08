651c351a92b23bb31965cc6c4477adb67349290c
hunter-s70 <serg.buhtoyarov@gmail.com>
2017-07-08 17:24:27 +0400

    add startIndex, rename variables and methods

--- js/app.js
+++ js/app.js
@@ -2,19 +2,30 @@
 
 const Dice = {};
 
+/*
+* STRUCTURE
+* 
+* |field
+* |- editable
+* |-- itemsWrap
+* |--- items
+* 
+*/
+
 Dice.roll_btn = document.querySelectorAll('.j-roll');
 Dice.add_field_btn = document.querySelectorAll('.j-add-field');
 Dice.uniquePrefixID = 'in_';
+Dice.startIndex = 1;
 
 Dice.data = {
-    [Dice.uniquePrefixID + 1]: ['Диван', 'Стол', 'Стул', 'Ванная', 'Пол'],
-    [Dice.uniquePrefixID + 2]: ['Собачки', 'Сверху', 'Бочком', 'Снизу', 'Новая']
+    [Dice.uniquePrefixID + Dice.startIndex]    : ['Диван', 'Стол', 'Стул', 'Ванная', 'Пол'],
+    [Dice.uniquePrefixID + Dice.startIndex + 1]: ['Собачки', 'Сверху', 'Бочком', 'Снизу', 'Новая']
 };
 
 
 // id unique number use in Dice.addField()
 Dice.count = (() => {
-    let index = 1;
+    let index = Dice.startIndex;
     return function() {
         return index++;
     }
@@ -28,7 +39,7 @@ Dice.roll = () => {
 
     for (let key in Dice.data) {
         roll_input = document.getElementById(key).querySelector('.j-roll-input');
-        randomNum = $$.randomNum(0, Dice.data[key].length-1);
+        randomNum = $$.randomNum(0, Dice.data[key].length - 1);
         roll_input.value = Dice.data[key][randomNum];
     }
 };
@@ -42,20 +53,20 @@ Dice.roll = () => {
 // add new input field to list
 Dice.addField = () => {
     let formLayout = document.querySelector('.l-fields'),
-        fieldInputID = Dice.uniquePrefixID + Dice.count(),
-        fieldInputClass = 'b-field j-field',
-        fieldInput;
+        fieldID = Dice.uniquePrefixID + Dice.count(),
+        fieldClass = 'b-field j-field',
+        fieldContent;
 
-    fieldInput  = '<input type="text" class="j-roll-input">';
-    fieldInput += '<button type="button" class="j-del-field">Del field</button>';
-    fieldInput += '<button type="button" class="j-edit-field">Edit field</button>';
+    fieldContent  = '<input type="text" class="j-roll-input">';
+    fieldContent += '<button type="button" class="j-del-field">Del field</button>';
+    fieldContent += '<button type="button" class="j-edit-field">Edit field</button>';
 
     $$.appendEl({
         elCreate: 'div',
         elParent: formLayout,
-        elClass: fieldInputClass,
-        elId: fieldInputID,
-        elContent: fieldInput
+        elClass: fieldClass,
+        elId: fieldID,
+        elContent: fieldContent
     });
 
     // initiate listeners to new input fields
@@ -82,26 +93,26 @@ Dice.deleteField = function() {
 // add editable tools to input in list
 Dice.addEditable = function() {
     let parentElement = this.parentNode,
-        editableClassName = 'b-field__edit',
-        editable = document.getElementsByClassName(editableClassName),
-        uniqueFieldID = parentElement.id,
-        editItemInput;
+        fieldID = parentElement.id,
+        editableClass = 'b-field__edit',
+        editable = document.getElementsByClassName(editableClass),
+        editableConent;
 
-    editItemInput  = '<input type="text" class="b-field__edit-input">';
-    editItemInput += '<button type="button" class="j-add-item">Add item</button>';
-    editItemInput += '<button type="button" class="j-close-edit">Close</button>';
+    editableConent  = '<input type="text" class="b-field__edit-input">';
+    editableConent += '<button type="button" class="j-add-item">Add item</button>';
+    editableConent += '<button type="button" class="j-close-edit">Close</button>';
 
     if (!editable.length) {
 
         $$.appendEl({
             elCreate: 'div',
             elParent: parentElement,
-            elClass: editableClassName,
-            elContent: editItemInput
+            elClass: editableClass,
+            elContent: editableConent
         });
 
         // like repeat item in items
-        Dice.renderItemsList(parentElement.querySelector('.' + editableClassName), uniqueFieldID);
+        Dice.renderItemsList(parentElement.querySelector('.' + editableClass), fieldID);
 
         // initiate listeners to new editable field
         Dice.initListeners();
@@ -110,35 +121,35 @@ Dice.addEditable = function() {
 
 
 // add items in DOM
-Dice.renderItemsList = (fieldEditWrap, uniqueFieldID) => {
-    let previewWrapClass = 'l-field__edit-preview-wrap',
-        previewWrap = fieldEditWrap.querySelector('.' + previewWrapClass);
+Dice.renderItemsList = (editable, fieldID) => {
+    let itemsWrapClass = 'l-field__edit-preview-wrap',
+        itemsWrap = editable.querySelector('.' + itemsWrapClass);
 
-    if (previewWrap) {
-        $$.delEl(previewWrap);
+    if (itemsWrap) {
+        $$.delEl(itemsWrap);
     }
 
     // create empty wrap for rolled items
     $$.appendEl({
         elCreate: 'div',
-        elParent: fieldEditWrap,
-        elClass: previewWrapClass
+        elParent: editable,
+        elClass: itemsWrapClass
     });
 
-    if (Dice.data[uniqueFieldID]) {
-        Dice.data[uniqueFieldID].forEach(function(item) {
-            let oncePreviewClass = 'b-field__edit-preview',
-                oncePreview = fieldEditWrap.querySelector('.' + previewWrapClass),
-                oncePreviewContent;
+    if (Dice.data[fieldID]) {
+        Dice.data[fieldID].forEach(function(itemValue) {
+            let itemClass = 'b-field__edit-preview',
+                item = editable.querySelector('.' + itemsWrapClass),
+                itemContent;
 
-            oncePreviewContent  = '<span class="b-field__edit-item">'+ item +'</span>';
-            oncePreviewContent += '<button type="button" class="j-del-item">Del item</button>';
+            itemContent  = '<span class="b-field__edit-item">'+ itemValue +'</span>';
+            itemContent += '<button type="button" class="j-del-item">Del item</button>';
 
             $$.appendEl({
                 elCreate: 'div',
-                elParent: oncePreview,
-                elClass: oncePreviewClass,
-                elContent: oncePreviewContent
+                elParent: item,
+                elClass: itemClass,
+                elContent: itemContent
             });
         });
     }
@@ -146,18 +157,18 @@ Dice.renderItemsList = (fieldEditWrap, uniqueFieldID) => {
 
 
 // funciton, wich add items in Dice.data
-Dice.addRollItem = function() {
+Dice.addItem = function() {
     let parentElement = this.parentNode,
         additionValue = parentElement.querySelector('.b-field__edit-input').value,
-        fieldName = parentElement.parentNode.id;
+        fieldID = parentElement.parentNode.id;
 
-    if (!Dice.data[fieldName]) Dice.data[fieldName] = [];
+    if (!Dice.data[fieldID]) Dice.data[fieldID] = [];
 
     if (additionValue.length) {
-        Dice.data[fieldName].push(additionValue);
+        Dice.data[fieldID].push(additionValue);
 
         // add items into view from Dice.data
-        Dice.renderItemsList(parentElement, fieldName);
+        Dice.renderItemsList(parentElement, fieldID);
 
         // initiate listeners to new editable field
         Dice.initListeners();
@@ -166,7 +177,7 @@ Dice.addRollItem = function() {
 
 
 // close editable tools
-Dice.closeEditable = function() {
+Dice.deleteEditable = function() {
     $$.delEl(this.parentNode);
 };
 
@@ -174,16 +185,16 @@ Dice.closeEditable = function() {
 // remove roll item
 Dice.deleteItem = function() {
     let parentElement = this.parentNode,
+        fieldID = parentElement.parentNode.parentNode.parentNode.id,
         itemValue = parentElement.querySelector('.b-field__edit-item').textContent,
-        fieldName = parentElement.parentNode.parentNode.parentNode.id,
-        itemIndex = Dice.data[fieldName].indexOf(itemValue);
+        itemIndex = Dice.data[fieldID].indexOf(itemValue);
 
     if (itemIndex > -1) {
-        Dice.data[fieldName].splice(itemIndex, 1);
+        Dice.data[fieldID].splice(itemIndex, 1);
     }
 
     // add items into view from Dice.data
-    Dice.renderItemsList(parentElement.parentNode.parentNode, fieldName);
+    Dice.renderItemsList(parentElement.parentNode.parentNode, fieldID);
 
     // initiate listeners to new editable field
     Dice.initListeners();
@@ -198,10 +209,10 @@ Dice.deleteItem = function() {
 // initiate listeners
 Dice.initListeners = () => {
     console.log('init');
-    $$.addListeners(document.querySelectorAll('.j-edit-field'), Dice.addEditable);
-    $$.addListeners(document.querySelectorAll('.j-add-item'), Dice.addRollItem);
-    $$.addListeners(document.querySelectorAll('.j-close-edit'), Dice.closeEditable);
     $$.addListeners(document.querySelectorAll('.j-del-field'), Dice.deleteField);
+    $$.addListeners(document.querySelectorAll('.j-edit-field'), Dice.addEditable);
+    $$.addListeners(document.querySelectorAll('.j-close-edit'), Dice.deleteEditable);
+    $$.addListeners(document.querySelectorAll('.j-add-item'), Dice.addItem);
     $$.addListeners(document.querySelectorAll('.j-del-item'), Dice.deleteItem);
 };
 
